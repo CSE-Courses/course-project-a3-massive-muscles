5f078b550f9b12661d123988c26e6970f7a583c8
IainT52 <iaintier@buffalo.edu>
2020-09-25 12:23:27 -0400

    Task#31 - Adding animation to homepage/added main csss file

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+VANTA.TOPOLOGY({
+  el: "#bgAnimation",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x072861,
+  backgroundColor: 0x4e9cf5
+});
