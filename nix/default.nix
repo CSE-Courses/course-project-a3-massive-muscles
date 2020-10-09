b8d9329379c83d12e413a5be9e3e89cbc54f261f
Edmund Wu <fangkazuto@gmail.com>
2020-10-08 20:06:02 -0400

    task(#35): nix: run postcss-preset-env on CSS
    This should expand compatibility with browsers (CSS) at least
    See https://github.com/csstools/postcss-preset-env for more information

--- nix/default.nix
+++ nix/default.nix
@@ -1,9 +1,31 @@
-{ stdenv }:
+{ stdenv, runCommand
+, postcss-cli
+, postcss-preset-env
+}:
 
 { src }:
+let
+  node_modules = runCommand "a3-massive-muscles-node_modules" {} ''
+    mkdir -p $out
+    ln -s ${postcss-preset-env} $out/postcss-preset-env
+  '';
+
+  # Basically like a LD_LIBRARY_PATH for Node.js if you don't know
+  NODE_PATH = stdenv.lib.concatStringsSep ":" (map
+    (drv: drv.out)
+    [ node_modules ]);
+in
 stdenv.mkDerivation {
   name = "a3-massive-muscles";
   inherit src;
+
+  configurePhase = ''
+    export NODE_PATH="${NODE_PATH}''${NODE_PATH:+:}$NODE_PATH"
+  '';
+
+  nativeBuildInputs = [
+    postcss-cli
+  ];
 
   buildPhase = ''
     # Copy over assets
@@ -14,5 +36,11 @@ stdenv.mkDerivation {
 
   installPhase = ''
     mv --verbose --no-target-directory public $out
+  '';
+
+  fixupPhase = ''
+    postcss "$out/**/*.css" \
+      --replace --no-map --verbose \
+      --use postcss-preset-env
   '';
 }
