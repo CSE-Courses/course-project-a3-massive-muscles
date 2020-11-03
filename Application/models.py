7c408fcab4a31823b8dd47d252e2fbe06bf0c755
Bootstarp <32107918+bootstrapcyber@users.noreply.github.com>
2020-11-02 20:17:19 -0500

    added more models and linked them to the database, and more database tests

--- Application/models.py
+++ Application/models.py
@@ -14,6 +14,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
+    BMIs = db.relationship('BMI', backref='user', lazy=True)
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
@@ -24,30 +25,35 @@ class User(db.Model, UserMixin):
 # measurement: measurement to be uploaded
 class BMI(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, primary_key=True)
+    date = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     measurement = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
         return f"BMI({self.date}, {self.measurement})"
 
-
-# creates a table for Calories data
-# key: is the date of submission
-# measurement: measurement to be uploaded
-class Calories(db.Model):
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, primary_key=True)
-    measurement = db.Column(db.Integer, nullable=False)
-
-    def __repr__(self):
-        return f"Calories({self.date}, {self.measurement})"
-
-
-# creates a table for Steps data
-# key: is the date of submission
-# measurement: measurement to be uploaded
-class Steps(db.Model):
-    date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, primary_key=True)
-    measurement = db.Column(db.Integer, nullable=False)
-
-    def __repr__(self):
-        return f"Steps({self.date}, {self.measurement})"
+#
+# # creates a table for Calories data
+# # key: is the date of submission
+# # measurement: measurement to be uploaded
+# class Calories(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     date = db.Column(db.DateTime, default=datetime.utcnow, primary_key=True, nullable=False)
+#     measurement = db.Column(db.Integer, nullable=False)
+#     calories_author = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#
+#     def __repr__(self):
+#         return f"Calorie({self.date}, {self.measurement})"
+#
+#
+# # creates a table for Steps data
+# # key: is the date of submission
+# # measurement: measurement to be uploaded
+# class Steps(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     date = db.Column(db.DateTime, default=datetime.utcnow, primary_key=True, nullable=False)
+#     measurement = db.Column(db.Integer, nullable=False)
+#     steps_author = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+#
+#     def __repr__(self):
+#         return f"Steps({self.date}, {self.measurement})"
--- database_tests.py
+++
@@ -1,8 +0,0 @@
-from Application.app import db, create_app, bcrypt
-from Application.models import BMI, Calories, Steps, User
-import time
-
-with create_app().app_context():
-    users = User.query.all()
-    print(users)
-
