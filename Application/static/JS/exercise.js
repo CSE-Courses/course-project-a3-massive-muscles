829dec92c8a037d1f18138a437f9ba86300b5fec
Kevin Brito <kevinbri@buffalo.edu>
2020-11-05 09:23:40 -0500

    task#100 created info buttons and exercise buttons for muscles

---
+++ Application/static/JS/exercise.js
@@ -0,0 +1,38 @@
+$(".exercises-front-deltoids").click(function(){
+    $(".modal-body").text("Chicken");
+  });
+$(".info-front-deltoids").click(function(){
+    $(".modal-body").text("The front and side deltoids (anterior and medial, respectively) are two very important parts of the shoulder, and are incredibly important when it relates to the general size and strength of your shoulders. Strong front and side deltoids will directly correlate to healthier shoulders and lead to increased functionality in the gym and everyday life.");
+  });
+
+$(".exercises-biceps").click(function(){
+    $(".modal-body").text("Chicken");
+  });
+$(".info-biceps").click(function(){
+    $(".modal-body").text("The biceps brachii, more commonly referred to as the 'biceps', is actually two different muscles that connect the shoulder and the elbow on the upper arm on the opposite side of the triceps. Your biceps have a 'short head' and a 'long head', so it's important to hit both of these parts equally in order to most effectively grow and train your biceps. The short head of the bicep is the inside part that is closer to your torso, and the long head parallel to the short head on the outside of your arm. Hammer Curls are a great exercise because it trains your long head, which is typically the more neglected part of your bicep. This exercise also pushes your tricep and bicep further apart which makes your arms look much bigger! This one exercise will change how your bicep AND tricep looks.");
+  });
+
+$(".exercises-chest").click(function(){
+    $(".modal-body").text("Chicken");
+  });
+$(".info-chest").click(function(){
+    $(".modal-body").text("The pectoralis major is one of three muscles on your chest, the pectoralis major being the largest of the three. The pectoralis major makes up the bulk of the chest muscle and it's used to flex and move the humerus, which is a large bone located on the upper arm. The pectoralis minor is a small, thin muscle located below the pectoralis major; it is connected to the ribs and functions to stabilize a large bone on the shoulder (scapula). The pectoralis fascia is another thin, small muscle that is located on top of the pectoralis major and it wraps around your chest to connect to your latissimus dorsi. These three parts are all important in their own way, but the pectoralis major is the biggest and thus usually targeted the most when training. Because of how large and dense the chest muscles are as a whole, you can surprisingly train this muscle group much more than you'd think. With that in mind, do make sure not to go overboard and over train yourself into an injury!");
+  });
+
+$(".exercises-obliques").click(function(){
+    $(".modal-body").text("Chicken");
+  });
+$(".info-obliques").click(function(){
+    $(".modal-body").text("The obliques are located on the sides of your abdominal muscles. Many people are unaware that the obliques are actually larger than the center abdominal muscles. The obliques main function is to pull the chest muscles down to compress the abdominal muscles. It's important for your abdominal muscles to be compressed because it acts like a spring to support your spine and upper body.");
+  });
+
+$(".exercises-quads").click(function(){
+    $(".modal-body").text("Chicken");
+  });
+$(".info-quads").click(function(){
+    $(".modal-body").text("The quadriceps is a large muscle group containing four different muscles on the front part of your thigh, opposite to your hamstrings. The quadriceps are crucial in walking, jumping, squatting, and any other activities involving movement with your legs.");
+  });
+
+
+
+  
