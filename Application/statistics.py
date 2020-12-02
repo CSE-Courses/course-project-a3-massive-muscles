972ca2cf9f4b36abcf79a8264fe068202121f676
Edmund Wu <fangkazuto@gmail.com>
2020-12-01 21:47:27 -0500

    task(#66): Application/statistics: create hooks for request

---
+++ Application/statistics.py
@@ -0,0 +1,28 @@
+from time import time
+
+from flask import g, request
+from flask_login import current_user
+
+import Application.models as DBModels
+from Application.app import db
+
+
+def pre_req():
+    g.req_tick = time.time()  # Track request "latency"
+
+
+def post_req(response):
+    tick = time.time()
+    req_latency = g.req_tick - tick
+    db.session.add(DBModels.RequestLatency(latency=req_latency))
+
+    user_id = current_user.get_id() if current_user.is_authenticated else -1
+    db.session.add(
+        DBModels.Statistics(user_id=user_id,
+                            url=request.url,
+                            path=request.path,
+                            protocol=request.method,
+                            user_agent=request.user_agent))
+
+    db.session.commit()
+    return response
